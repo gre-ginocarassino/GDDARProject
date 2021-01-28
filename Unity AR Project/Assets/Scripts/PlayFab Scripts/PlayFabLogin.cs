d991bbb861c33824ac264793caf7e50b1d435044
Gino Carassino <ec5273q@greenwich.ac.uk>
2021-01-28 18:38:16 +0100

    PlayFab

---
+++ Unity AR Project/Assets/Scripts/PlayFab Scripts/PlayFabLogin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+
+public class PlayFabLogin : MonoBehaviour
+{
+
+    private string userEmail;
+    private string userPassword;
+
+    public void Start()
+    {
+        //If the Title ID is null or empty, goes in here
+        if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
+        {
+            PlayFabSettings.TitleId = "2B652"; 
+        }
+        //var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true }; //request to log in
+        //PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure); //submit the request
+
+        
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+    }
+
+    private void OnLoginFailure(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+}
