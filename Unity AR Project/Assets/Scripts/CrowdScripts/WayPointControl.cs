b836017cb7409cc7ac0e6724774b73e107b2a8af
Jay <jz7767a@gre.ac.uk>
2021-02-02 14:16:44 +0000

    Just an update on crowd test
    It is very lagging as a result of testing. 100 models with total of 16000 vertices.

---
+++ Unity AR Project/Assets/Scripts/CrowdScripts/WayPointControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointControl : MonoBehaviour
+{
+    public List<Transform> waypoints = new List<Transform>();
+    private Transform targetWaypoint;
+    private int targetWaypointIndex = 0;
+    private float minDistance = 0.1f;
+    private int lastWaypointIndex;
+
+    float movementSpeed = 0.05f;
+
+    void Start()
+    {
+        lastWaypointIndex = waypoints.Count - 1;
+        targetWaypoint = waypoints[targetWaypointIndex];
+    }
+
+  
+    void Update()
+    {
+        float movementStep = movementSpeed * Time.deltaTime;
+
+        float distance = Vector3.Distance(transform.position, targetWaypoint.position);
+        CheckDistanceToWayPoint(distance);
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, movementStep);
+
+        
+    }
+
+    void CheckDistanceToWayPoint(float currentDistance)
+    {
+        if (currentDistance <= minDistance)
+        {
+            targetWaypointIndex++;
+            UpdateTargetWayPoint();
+        }
+    }
+
+    void UpdateTargetWayPoint()
+    {
+        if(targetWaypointIndex > lastWaypointIndex)
+        {
+            targetWaypointIndex = 0;
+        }
+        targetWaypoint = waypoints[targetWaypointIndex];
+    }
+}
