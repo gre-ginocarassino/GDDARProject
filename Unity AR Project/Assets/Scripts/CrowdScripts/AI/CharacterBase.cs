c1fbf31a3754eba60b9fd16f69f12b55f19f4a60
Jay <jz7767a@gre.ac.uk>
2021-03-09 14:45:41 +0000

    Auto stash before merge of "main" and "origin/main"

--- Unity AR Project/Assets/Scripts/CrowdScripts/AI/CharacterBase.cs
+++ Unity AR Project/Assets/Scripts/CrowdScripts/AI/CharacterBase.cs
@@ -26,13 +26,15 @@ public class CharacterBase : MonoBehaviour
     private void Start()
     {
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
-        //targetLocation = endNode._endNode.nodePoint.transform;
         _animator = GetComponent<Animator>();
 
         if (targetLocation == null)
         {
             targetLocation = endNode._endNode.nodePoint.transform;
         }
+        //randomLocation = Random.Range(0, Spawn._Spawn.allDirection.Length);
+        //randomDistance = Random.Range(0, 1f);
+        //randomTime = Random.Range(3, 7f);
 
         path = new NavMeshPath();
     }
@@ -48,9 +50,46 @@ public class CharacterBase : MonoBehaviour
             NavMesh.CalculatePath(transform.position, targetLocation.position, NavMesh.AllAreas, path);
             //Debug.LogError("no it does not have path");
         }
+
         _navMeshAgent.SetDestination(targetLocation.position);
         _animator.SetBool("shouldAnimating", true);
+
+        //if (randomTime <= 0)
+        //{
+        //    chooseAnotherRoute();
+        //}
+
+        //else
+        //{
+        //    randomTime -= Time.deltaTime;
+        //}
+        ////if distance less random distance, execute following lines
+        //if (Vector3.Distance(transform.position, Spawn._Spawn.allDirection[randomLocation].position) <= randomDistance)
+        //{
+        //    _animator.SetBool("shouldAnimating", false);
+
+        //    if (waitTime <= 0)
+        //    {
+                
+        //        randomLocation = Random.Range(0, Spawn._Spawn.allDirection.Length);
+        //        waitTime = startWaitTime;
+        //    }
+            
+        //    else
+        //    {
+        //        waitTime -= Time.deltaTime;
+        //    }
+        //}
+
+        
     }
+
+    //private void chooseAnotherRoute()
+    //{
+    //    randomLocation = Random.Range(0, Spawn._Spawn.allDirection.Length);
+    //    randomDistance = Random.Range(0, 1f);
+    //    randomTime = Random.Range(3, 7f);
+    //}
 
     public void chooseAction(int action)
     {
@@ -68,6 +107,8 @@ public class CharacterBase : MonoBehaviour
     public void setInactive()
     {
         gameObject.SetActive(false);
+
+        
     }
 
     
--- Unity AR Project/Assets/Scripts/CrowdScripts/AI/navigationSign.cs
+++ Unity AR Project/Assets/Scripts/CrowdScripts/AI/navigationSign.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class navigationSign : MonoBehaviour
 {
     private int randomSelection;
+
+    public int threshold;
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<CharacterBase>(out CharacterBase _characterBase))
         {
             randomSelection = Random.Range(0, 2);
-            //Debug.Log(randomSelection);
-            _characterBase.chooseAction(randomSelection);
+            if (threshold < 10 && randomSelection == 0)
+            {
+                //Debug.Log(randomSelection);
+                _characterBase.chooseAction(randomSelection);
+                threshold += 1;
+            }
         }
     }
 }
